36696d600fcd99fc84b7d267fe477ba39a57f4ae
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-11-04 16:07:14 +0100

    Update Repo.js

--- src/components/ReposResults/Repo.js
+++ src/components/ReposResults/Repo.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image, Icon } from 'semantic-ui-react';
+import {
+  Card, Image, Icon, Popup,
+} from 'semantic-ui-react';
 
 const Repo = ({
-  name, description, owner, watchers, html_url,
+  name, description, owner, watchers, html_url, stargazers_count,
 }) => (
-  <Card href={html_url}>
-    <Image src={owner.avatar_url} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{name}</Card.Header>
-      <Card.Meta>
-        {owner.login}
-      </Card.Meta>
-      <Card.Description>
-        {description}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <Icon name="eye" />
-      {watchers} Watchers
-    </Card.Content>
-  </Card>
+  <Popup trigger={
+    (
+      <Card href={html_url} target="blank">
+        <Image src={owner.avatar_url} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{name}</Card.Header>
+          <Card.Meta>
+            {owner.login}
+          </Card.Meta>
+          <Card.Description>
+            {description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Icon name="eye" />
+          {watchers} Watchers
+        </Card.Content>
+      </Card>
+    )
+  }
+  >
+    <Popup.Header>Star :</Popup.Header>
+    <Popup.Content>
+      {stargazers_count}
+    </Popup.Content>
+  </Popup>
 );
 
 Repo.propTypes = {
